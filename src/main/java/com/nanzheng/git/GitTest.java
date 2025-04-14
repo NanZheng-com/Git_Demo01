c253d7219013b6ad2d787d4c02466c45a0124b42
Kido <1935230850@qq.com>
2025-04-14 21:22:30 +0800

    hot-fix second commit

--- src/main/java/com/nanzheng/git/GitTest.java
+++ src/main/java/com/nanzheng/git/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 
         System.out.println("hello git2!");
 
-        System.out.println("hello branch:hot-fix");
-
+        System.out.println("hello branch:hot-fix!");
+        System.out.println("hello branch:hot-fix test!");
     }
 }
